0c231615467fd3cd1bfcf1836b2ec30fb5855017
Tanuri Bawanya <tanuribawanya@gmail.com>
2024-12-17 15:24:06 +0530

    Add untracked files

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import './navstyles.css';
 const NavigationBar = () => {
     return(
         <div>
+            <nav>
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>About Us</li>
+                <li><a href="#"></a>Contact Us</li>
+                <li><a href="#"></a>Pet Profile</li>
 
+            </nav>
         </div>
     );
 };
